2d1de8b23cc919f5c6a7acb613fa049711ecb1b0
Paulo L F Casaretto <817039+pcasaretto@users.noreply.github.com>
2017-10-30 20:34:30 -0200

    Add concurrent capabilities

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :dependencies [
+                 [org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/core.async "0.3.443"]
+                 ]
   :main ^:skip-aot clojure-chat.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_chat/core.clj
+++ src/clojure_chat/core.clj
@@ -1,7 +1,48 @@
 (ns clojure-chat.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+    [clojure.core.async :as async]
+    [clojure.java.io :as io])
+  (:import (java.net ServerSocket)))
+
+(defn echo-service
+  [in out]
+  (async/go-loop []
+    (let [msg (async/<! in)]
+      (async/>! out msg)
+      (recur))))
+
+(defn line-reader
+  [sock]
+  (let [in (async/chan)
+        reader (io/reader sock)]
+    (async/go-loop []
+      (let [msg (.readLine reader)]
+        (async/>! in msg))
+      (recur))
+    in))
+
+(defn echo-writer
+  [sock]
+  (let [out (async/chan)
+        writer (io/writer sock)]
+    (async/go-loop []
+      (let [msg (async/<! out)]
+        (.write writer (str "Echoing: " msg)))
+      (.flush writer)
+      (recur))
+    out))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println "Starting server ...")
+  (let [server (ServerSocket. 32665)]
+      (loop []
+        (let [sock (.accept server)]
+        (println (format "Listening on socket %s ..." sock))
+        (async/go
+          (echo-service
+            (line-reader sock)
+            (echo-writer sock)))
+        (recur )))
+    (.join (Thread/currentThread))))
